c67ba017694eef66f34b6bad461af565265c2ee9
HardPlant <abc7988se@naver.com>
2019-05-29 14:36:22 +0900

    move and update

---
+++ AutoSchedule/0.IAM.tf
@@ -0,0 +1,60 @@
+### IAM Role and Policy ###
+# Allows Lambda function to describe, stop and start EC2 instances
+
+# sts:AssumeRole: 임시 credential
+# lambda를 사용할 수 있게 하는 권한을 얻어음 (AWS Security Token Service)
+resource "aws_iam_role" "ec2_start_stop_scheduler" {
+  name = "ec2_start_stop_scheduler"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# 
+data "aws_iam_policy_document" "ec2_start_stop_scheduler" {
+  statement = [
+    {
+      actions = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+      resources = [
+        "arn:aws:logs:*:*:*",
+      ]
+    },
+    {
+      actions = [
+        "ec2:Describe*",
+        "ec2:Stop*",
+        "ec2:Start*"
+      ]
+      resources = [
+          "*",
+      ]
+    }
+  ]
+}
+
+resource "aws_iam_policy" "ec2_start_stop_scheduler" {
+  name = "ec2_access_scheduler"
+  path = "/"
+  policy = "${data.aws_iam_policy_document.ec2_start_stop_scheduler.json}"
+}
+# arn : Amazon Resource Name
+resource "aws_iam_role_policy_attachment" "ec2_access_scheduler" {
+  role       = "${aws_iam_role.ec2_start_stop_scheduler.name}"
+  policy_arn = "${aws_iam_policy.ec2_start_stop_scheduler.arn}"
+}
