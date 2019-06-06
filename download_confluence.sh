0bb2c5a17e31cfe94b79c450d30cdb1f07cf14bb
HardPlant <abc7988se@naver.com>
2019-06-06 18:07:53 +0900

    ssh

--- download_confluence.sh
+++ download_confluence.sh
@@ -2,3 +2,6 @@
 
 mkdir /var/confluence
 wget https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-6.15.4-x64.bin -P /var/confluence
+cd /var/confluence
+chmod u+x atlassian-confluence-6.15.4-x64.bin
+./atlassian-confluence-6.15.4-x64.bin
