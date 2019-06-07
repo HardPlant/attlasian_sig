931370bb65fb23280caa58440153036a40f1fa8a
HardPlant <abc7988se@naver.com>
2019-06-07 10:31:09 +0900

    jira

--- download_jira.sh
+++ download_jira.sh
@@ -2,3 +2,6 @@
 
 mkdir /var/jira
 wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-8.2.1-x64.bin -P /var/jira
+cd /var/jira
+chmod u+x atlassian-jira-software-8.2.1-x64.bin
+./atlassian-jira-software-8.2.1-x64.bin
