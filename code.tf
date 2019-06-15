a749f2f0c14d3131ba229acd39c9ee54ccb96433
HardPlant <abc7988se@naver.com>
2019-06-15 21:36:43 +0900

    isolated db with ec2

--- code.tf
+++ code.tf
@@ -228,17 +228,3 @@ resource "aws_instance" "jira" {
   #    ]
   # }
 }
-
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  name                 = "confjira_db"
-  username             = "tta"
-  password             = "${var.db_key}"
-  parameter_group_name = "default.mysql5.7"
-  publicly_accessible = true
-  # db_subnet_group_name = "${aws_db_subnet_group.default.name}"
-}
